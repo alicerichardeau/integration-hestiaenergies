0e6e6969d8bd52db338b81b82482b51bb6b3d56e
alice_richardeau <alicerichardeau@outlook.fr>
2022-01-07 10:14:33 +0100

    ok

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// url
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('.navbar-nav a').forEach(link => {
+    // if url add class active
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add('active');
+    }
+})
