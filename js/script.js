0cfb039f7a20e82d63d8d7413e6e07875ab78ac1
alrich-456 <alicerichardeau@outlook.com>
2022-01-27 15:34:52 +0100

    update animation css

--- js/script.js
+++ js/script.js
@@ -9,3 +9,31 @@ const navLinks = document.querySelectorAll('.navbar-nav a').forEach(link => {
         link.classList.remove('active');
     }
 })
+
+// animation css - reveal elements
+// ###############################
+
+// define options
+const ratio = .5
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: ratio
+}
+
+// add class reveal-visible when ratio is good
+const handleIntersect = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.target.classList.add('reveal-visible')
+      observer.unobserve(entry.target)
+    }
+  })
+}
+
+// loop foreach element who have reveal class css reveal
+  const observer = new IntersectionObserver(handleIntersect, options) 
+  document.querySelectorAll('.reveal').forEach(function(r) {
+    observer.observe(r)
+  })
+  
